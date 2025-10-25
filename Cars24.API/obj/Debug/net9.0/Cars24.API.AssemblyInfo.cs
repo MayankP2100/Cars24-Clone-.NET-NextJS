9ea225caa057d6f16acc54f58d18c131d73412ff
Mayank Pal <palmayank2100@gmail.com>
2025-10-25 16:18:23 +0530

    Update BASE_URL to production endpoint in utils.ts

--- Cars24.API/obj/Debug/net9.0/Cars24.API.AssemblyInfo.cs
+++ Cars24.API/obj/Debug/net9.0/Cars24.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cars24.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b7b4ff7679e429975bc2b421cb3f017ae971811")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43265ab955e03a0a849a7d2966ee10e56d0e5387")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cars24.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cars24.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
